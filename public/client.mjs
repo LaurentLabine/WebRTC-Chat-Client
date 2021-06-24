4b98c191dd94284613f77bf1a60d0558b1d32527
Laurent Labine <laurentlabine@gmail.com>
2021-06-24 13:13:38 +0200

    Moved Web RTC stuff to a class component

--- public/client.mjs
+++ public/client.mjs
@@ -1,5 +1,7 @@
 import { makeId } from './util/makeId.js'
-import { initializePeerConnection, initiateCall } from './util/rtcPeerConnection.js'
+// import { initializePeerConnection, initiateCall } from './util/rtcPeerConnection.js'
+// import RTCConnection from '/'
+import RTCConnection from './models/rtcConnection.js'
 
 let localStream = null;
 let webcamRunning = false;
@@ -11,11 +13,11 @@ const callButton = document.getElementById('callButton');
 const callIDInput = document.getElementById('callIDInput');
 const debugButton = document.getElementById('debugButton')
 
-const peerConnection = initializePeerConnection();
+const webRTCConnection = new RTCConnection();
 
 //Debugging button 
 debugButton.addEventListener('click', () => {
-  console.log(peerConnection)
+  // console.log(peerConnection)
 })
 
 callButton.onclick = async () => {
@@ -23,7 +25,7 @@ callButton.onclick = async () => {
   const callID = callIDInput.value === '' ? makeId() : callIDInput.value
   callIDInput.innerText = callID;
   // Initiate a call passing along the ID.
-  initiateCall(callID)
+  webRTCConnection.initiateCall(callID)
 }
 
 // 1. Setup Webcam Stuff
@@ -32,19 +34,20 @@ webcamButton.onclick = async () => {
   webcamRunning = !webcamRunning;
     callButton.disabled = !callButton.disabled;
 
-  if(localStream){
-    localStream.getTracks().forEach(track => track.stop())
-    localStream = null
-    localVideo.srcObject = null
-  }
-  else{
+  // if(localStream){
+  //   localStream.getTracks().forEach(track => track.stop())
+  //   localStream = null
+  //   localVideo.srcObject = null
+  // }
+  // else{
 
-    localStream = await navigator.mediaDevices.getUserMedia({
+    webRTCConnection.localStream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true, 
     });
 
-    peerConnection.addStream(localStream)
-    localVideo.srcObject = localStream;
+    // peerConnection.lo
+
+    webRTCConnection.addLocalStream()
+    localVideo.srcObject = webRTCConnection.localStream;
   }
-};
