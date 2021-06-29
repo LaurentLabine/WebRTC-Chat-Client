d9119a9425b09ee2727746cd931c367c31f74497
Laurent Labine <laurentlabine@gmail.com>
2021-06-29 15:09:10 +0200

    renamed events to standardize again

--- public/src/util/signalingServer.js
+++ public/src/util/signalingServer.js
@@ -7,55 +7,52 @@ const startSignalingServer = (httpServer) => {
 io.on('connection', socketIO => {
 
   // Take the passed ID from the browser
-  socketIO.on('initiateCall', callID =>{
-    console.log('initiateCall')
+  socketIO.on('registercall', callID =>{
 
     // If the call already exists
     if(calls[callID]){
       // If the user is attempting to enter a call he's already registered in, do nothing.
       if(calls[callID].indexOf(socketIO.id) >= 0){
-        // Otherwise, Second User just joined the call.  Add the user and emit 
+        // Otherwise, Second User just joined the call.  Add the user and emit a 'otheruserjoined'
       } else {
-
         calls[callID].push(socketIO.id)
-        console.log('Server - ' + socketIO.id + ' Joined')
-        // console.log(typeof(socketIO.id))
-        // socketIO.emit('other user joined', socketIO.id)
-        io.to(calls[callID][0]).emit('OtherJoined', {caller: calls[callID][0], target:socketIO.id})
+        io.to(calls[callID][0]).emit('otheruserjoined', {caller: calls[callID][0], target:socketIO.id})
       }
 
       // Otherwise we register the call
     } else {
       //Otherwise create and register it with the unique socket id
       calls[callID] = [socketIO.id]
-      console.log('Server - Call Created')
     }
-    console.log(calls)
 
       // when offer gets fired
-    socketIO.on('OfferCreated', payload => {
-      io.to(payload.target).emit('ReceivedOffer', payload);
+    socketIO.on('offercreated', payload => {
+      io.to(payload.target).emit('receivedoffer', payload);
   });
 
   // the payload contains who we are as a user and the 'offer' object.
 
   // listen for the answer event
-  socketIO.on('answer', payload => {
-    console.log('received answer')
-    io.to(payload.caller).emit('ReceivedAnswer', payload);
+  socketIO.on('answeremitted', payload => {
+    io.to(payload.caller).emit('receivedanswer', payload);
   });
 
    // each peer will come up with an 'ice server'
-   socketIO.on('new-ice-candidate', incoming => {
+   socketIO.on('newicecandidate', incoming => {
 
     //Determine which candidate to send this to
     if(calls[callID].indexOf(socketIO.id) === 0){
-      io.to(calls[callID][1]).emit('remote-ice-candidate', incoming);
+      io.to(calls[callID][1]).emit('remoteicecandidate', incoming);
     } else {
-      io.to(calls[callID][0]).emit('remote-ice-candidate', incoming);
+      io.to(calls[callID][0]).emit('remoteicecandidate', incoming);
     }
   });
   })
+
+  socketIO.on('disconnect', socketIO => {
+    console.log('user disconnected')
+  })
+
 })
 }
 
